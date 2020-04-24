8234a5a5e905f85a1e81e9d790de0e24ce30e85a
alevieiramoreira <alessandra-vieira-moreira@hotmail.com>
2020-04-23 22:13:07 -0300

    adiciona estrutura do projeto

---
+++ src/config/auth.ts
@@ -0,0 +1,6 @@
+export default {
+  jwt: {
+    secret: 'a72ce136ceec7c54b182f47da709d14e',
+    expiresIn: '1d',
+  },
+};
---
+++ src/services/CreateAppointmentService.ts
@@ -0,0 +1,48 @@
+import { startOfHour } from 'date-fns';
+import { getCustomRepository } from 'typeorm';
+
+import Appointment from '../models/Appointment';
+import AppointmentsRepository from '../repositories/AppointmentsRepository';
+
+// Todo service tem apenas um único método
+
+/* Dentro do execute é importante tratar:
+    - Recebimento das informações
+    - Tratativa de erros/exceções
+    - Acesso ao repository
+*/
+
+interface RequestDTO {
+  provider_id: string;
+  date: Date;
+}
+
+class CreateAppointmentService {
+  public async execute({
+    provider_id,
+    date,
+  }: RequestDTO): Promise<Appointment> {
+    const appointmentsRepository = getCustomRepository(AppointmentsRepository);
+
+    const appointmentDate = startOfHour(date);
+
+    const findAppointmentInSameDate = await appointmentsRepository.findByDate(
+      appointmentDate,
+    );
+    // achando datas iguais
+    if (findAppointmentInSameDate) {
+      throw Error('Horário já marcado');
+    }
+
+    const appointment = appointmentsRepository.create({
+      provider_id,
+      date: appointmentDate,
+    });
+
+    await appointmentsRepository.save(appointment);
+
+    return appointment;
+  }
+}
+
+export default CreateAppointmentService;
